f48763d17f4f722a2b4fab9cc3264ea9d61bb3ec
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2025-09-17 20:52:59 -0700

    Removed default home screen code

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,18 @@
 import { useState } from 'react'
 import BARTLogo from './assets/BARTLogo.png'
 import UCMLogo from './assets/UCMLogo.png'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={BARTLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={UCMLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={BARTLogo} className="logo" alt="Vite logo" />
+        <img src={UCMLogo} className="logo react" alt="React logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
