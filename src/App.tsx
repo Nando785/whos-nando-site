587b6aecc8a2bad2c5a1a7d6dae1df8d5d360d20
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2025-09-17 17:59:16 -0700

    Integrated logo usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
+import {BARTLogo, STEMTutoringLogo, UCMLogo} from './assets'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,10 +12,10 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={BARTLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={UCMLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+import BARTLogo from './BARTLogo.png';
+import STEMTutoringLogo from './STEMTutoringLogo.png';
+import UCMLogo from './UCMLogo.png';
+
+export { BARTLogo, STEMTutoringLogo, UCMLogo };
