bad151c6c3353335eb368c970c8ea10cd76bf2e3
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2025-09-20 17:19:11 -0700

    Started website header implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import BARTLogo from './assets/BARTLogo.png'
-import UCMLogo from './assets/UCMLogo.png'
-import STEMLogo from './assets/STEMTutoringLogo.png'
+// import BARTLogo from './assets/BARTLogo.png'
+// import UCMLogo from './assets/UCMLogo.png'
+// import STEMLogo from './assets/STEMTutoringLogo.png'
 import './App.css'
 
-import JobCard from './components/job-card'
+// import JobCard from './components/job-card'
+
+// ==================================================
+// Temp Code Snippets
+
+// <img src={BARTLogo} className="logo" alt="Vite logo" />
+// <img src={UCMLogo} className="logo react" alt="React logo" />
+// <img src={STEMLogo} className="logo react" alt="React logo" />
+// <JobCard></JobCard>
+// ==================================================
 
 function App() {
 
   return (
     <>
-      <div>
-        <img src={BARTLogo} className="logo" alt="Vite logo" />
-        <img src={UCMLogo} className="logo react" alt="React logo" />
-        <img src={STEMLogo} className="logo react" alt="React logo" />
-        <JobCard></JobCard>
+      <div className="header">
+        <div className="header-content">
+          <h1>Who's Nando?</h1>
+        </div>
       </div>
     </>
   )
