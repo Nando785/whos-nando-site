6a38474f6b4937c3ab48a5262f0c47a31bb25acb
Fernando Rodriguez Martinez <Fernandormartinez21@gmail.com>
2026-01-06 21:09:06 -0800

    Made JobCard component modular
    Changed JobCard element description, title, and image into a variable input instead of hard coded.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-// import BARTLogo from './assets/BARTLogo.png'
-// import UCMLogo from './assets/UCMLogo.png'
-// import STEMLogo from './assets/STEMTutoringLogo.png'
+import BARTLogo from './assets/BARTLogo.png'
+import UCMLogo from './assets/UCMLogo.png'
+import STEMLogo from './assets/STEMTutoringLogo.png'
 import './App.css'
 
-// import JobCard from './components/job-card'
-
-// ==================================================
-// Temp Code Snippets
-
-// <img src={BARTLogo} className="logo" alt="Vite logo" />
-// <img src={UCMLogo} className="logo react" alt="React logo" />
-// <img src={STEMLogo} className="logo react" alt="React logo" />
-// <JobCard></JobCard>
-// ==================================================
+import JobCard from './components/job-card'
 
 function App() {
 
@@ -22,6 +13,26 @@ function App() {
         <div className="header-content">
           <h1>Who's Nando?</h1>
         </div>
+      </div>
+
+      <div>
+        <JobCard
+            imageSrc={BARTLogo}
+            title="Full-Stack Development Intern"
+            description="Built a full-stack Django application that used TensorFlow and OpenCV to analyze BART track footage and automatically detect infrastructure defects. Designed RESTful APIs connecting a custom-trained CNN to the frontend, delivering a seamless and reliable user experience. Collaborated with a six-person team to prototype a system that improved inspection efficiency and accuracy."
+          />
+
+          {/* <JobCard
+            imageSrc={STEMLogo}
+            title="Computer Science & Engineering Tutor"
+            description="Tutored undergraduate computer science students in both lower- and upper-division courses, covering core topics across Python, C, and C++. Specialized in breaking down complex concepts like data structures, memory management, and algorithms into clear, approachable explanations."
+          />
+
+          <JobCard
+            imageSrc={UCMLogo}
+            title="Remidial Tutor I - Group Sessions"
+            description="Led weekly live Zoom sessions for incoming students, teaching foundational programming and math concepts through hands-on exercises, algorithm walkthroughs, and live debugging. Developed and delivered Python curriculum with faculty, covering core concepts like loops, conditionals, and functions. Mentored students through individualized feedback while tracking engagement and performance to support data-driven program outcomes."
+          /> */}
       </div>
     </>
   )
--- src/components/job-card.tsx
+++ src/components/job-card.tsx
@@ -4,8 +4,8 @@ import './job-card.css'
 interface JobCardProps {
   imageSrc?: string
   imageAlt?: string
+  description?: string
   title?: string
-  body?: string
 }
 
 const JobCard: React.FC<JobCardProps> = (props) => {
@@ -17,8 +17,8 @@ const JobCard: React.FC<JobCardProps> = (props) => {
         className='job-card-logo'
       />
 
-      <span className="job-card-title">{"Full-Stack Development Intern"}</span>
-      <span className="job-card-body">{"Developed a full-stack Django application that integrated machine learning (TensorFlow) and computer vision (OpenCV) to analyze BART track footage and automatically detect infrastructure defects, demonstrating expertise across both frontend and backend development. I designed and implemented RESTful API routes to connect the user interface with a custom-trained CNN, ensuring seamless, robust feature integration. Working closely with a six-member team, I analyzed existing inspection workflows and delivered a prototype that significantly improved inspection efficiency and accuracy showcasing not only my technical ability but also my collaboration and performance optimization skills."}</span>
+      <span className="job-card-title">{props.title}</span>
+      <span className="job-card-body">{props.description}</span>
     </div>
   )
 }
